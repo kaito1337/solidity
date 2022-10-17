03c8379214f035bd3c4b3133ea0c1c2fbc4be6f3
kaito1337 <90248937+kaito1337@users.noreply.github.com>
2022-10-17 13:05:36 +0300

    Create build.js

---
+++ build.js
@@ -0,0 +1,8 @@
+let minerAcc = personal.newAccount("123");
+for (let i = 0; i < eth.accounts.length; i++) {
+    const account = eth.accounts[i];
+    personal.unlockAccount(account, "123", 0);
+}
+
+miner.setEtherbase(minerAcc);
+miner.start();
