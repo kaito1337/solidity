f9f8a428da08d70359ffa0c5d65f51f42d95ba71
kaito1337 <90248937+kaito1337@users.noreply.github.com>
2022-10-18 16:43:44 +0300

    Update token.sol
    kekich

--- token.sol
+++ token.sol
@@ -3,36 +3,41 @@ pragma solidity ^0.8.7;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Contract is ERC20("cmonToken", "CMT"){
+
     address admin = msg.sender;
     address[] whitelist;
     uint256 etap1 = block.timestamp;
     uint256 etap2 = etap1 + 3 minutes;
     uint256 etap3 = etap2 + 2 minutes;
     uint256 etap4 = etap3 + 5 minutes;
+
+    address inv1 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    address inv2 = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+    address inv3 = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
+    address rab1 = 0x617F2E2fD72FD9D5503197092aC168c91465E7f2;
+    uint256 summa = 30*(10**decimals());
+
     struct User{
         string login;
         address wallet;
         uint256 balance;
         bool white;
+        bool razrab;
     }
 
-    constructor() payable {
-        _mint(admin, 1000000);
-        address inv1 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-        address inv2 = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
-        address inv3 = 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB;
-        address sale = 0x617F2E2fD72FD9D5503197092aC168c91465E7f2;
-        transfer(inv1, 200000);
-        transfer(inv2, 100000);
-        transfer(inv3, 50000);
-        transfer(sale, 200000);
-        userMap[inv1] = User("inv1", inv1, balanceOf(inv1), false);
+    constructor(){
+        uint256 dec = 10**decimals();
+        _mint(admin, 650000*dec);
+        _mint(inv1, 200000*dec);
+        _mint(inv2, 100000*dec);
+        _mint(inv3, 50000*dec);
+        userMap[inv1] = User("inv1", inv1, balanceOf(inv1), false, false);
         userLoginMap["inv1"] = inv1;
         userPassMap["inv1"] = "123";
-        userMap[inv1] = User("inv2", inv2, balanceOf(inv2), false);
+        userMap[inv1] = User("inv2", inv2, balanceOf(inv2), false, false);
         userPassMap["inv2"] = "123";
         userLoginMap["inv2"] = inv2;
-        userMap[inv1] = User("inv3", inv3, balanceOf(inv3), false);
+        userMap[inv1] = User("inv3", inv3, balanceOf(inv3), false, false);
         userPassMap["inv3"] = "123";
         userLoginMap["inv3"] = inv3;
     }
@@ -41,9 +46,10 @@ contract Contract is ERC20("cmonToken", "CMT"){
     mapping(string => address) public userLoginMap;
     mapping(string => string) public userPassMap;
     mapping(address => bool) public userWhiteMap;
+    mapping(address => bool) public userIsRab;
 
     function register(string memory _login, string memory _password) public{
-        userMap[msg.sender] = User(_login, msg.sender, 0, false);
+        userMap[msg.sender] = User(_login, msg.sender, balanceOf(msg.sender), false, false);
         userPassMap[_login] = _password;
     }
 
@@ -60,24 +66,37 @@ contract Contract is ERC20("cmonToken", "CMT"){
         if(_solut){
             userMap[whitelist[_index]].white = true;
             whitelist.push(whitelist[_index]);
+            userWhiteMap[whitelist[_index]] = true;
         }
         delete whitelist[_index];
     }
 
-    function buyToken(uint256 _amount) public payable isWhite{
-        uint256 tokenPrice = 0.001 ether;
-        address sale = 0x617F2E2fD72FD9D5503197092aC168c91465E7f2;
-        if(block.timestamp >= etap4){
-            tokenPrice = 0.0075 ether;
-            require(msg.sender.balance >= tokenPrice* _amount, "Malo deneg");
-            payable(admin).transfer(_amount * tokenPrice);
-            require(balanceOf(sale) >= _amount, "Malo tokenov");
-            transferFrom(sale, msg.sender, _amount);
+    function takeTokens() public isRab{
+        require(block.timestamp == 4 minutes || block.timestamp == 7 minutes || block.timestamp == 10 minutes || block.timestamp == 13 minutes, "Rano eshe");
+        transferFrom(admin, msg.sender, 10000*(10**decimals()));
+        userMap[msg.sender].balance = balanceOf(msg.sender);
+    }
+
+    function buyToken() public payable isWhite{
+        uint256 tokenPrice = 1 ether;
+        if(block.timestamp < etap4 && summa >= msg.value/tokenPrice * decimals()){
+            payable(admin).transfer(msg.value);
+            transferFrom(admin, msg.sender, msg.value/tokenPrice * decimals());
+            summa -= msg.value/tokenPrice*decimals();
         }
-        require(msg.sender.balance >= tokenPrice* _amount, "Malo deneg");
-        require(balanceOf(sale) >= _amount, "Malo tokenov");
-        payable(admin).transfer(_amount * tokenPrice);
-        transferFrom(sale, msg.sender, _amount);
+        else{
+            // анлаки братан токенов нет
+        }
+        if(block.timestamp >= etap4){
+            tokenPrice = 2 ether;
+        }
+        payable(admin).transfer(msg.value);
+        transferFrom(admin, msg.sender, msg.value/tokenPrice* decimals());
+        userMap[msg.sender].balance = balanceOf(msg.sender);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 18;
     }
 
     modifier isAdmin(){
@@ -87,6 +106,11 @@ contract Contract is ERC20("cmonToken", "CMT"){
 
     modifier isWhite(){
         require(userWhiteMap[msg.sender] == true || block.timestamp >= etap4, "You are not white");
+        _;
+    }
+
+    modifier isRab() {
+        require(userIsRab[msg.sender] == true);
         _;
     }
 
