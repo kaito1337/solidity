e84e1d97d54a955efec6f94fcfefaf67d846f2e7
kaito1337 <90248937+kaito1337@users.noreply.github.com>
2022-12-27 14:41:14 +0300

    Update build.js

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-let minerAcc = personal.newAccount("123");
+let minerAcc = "0x941710e12b67f13090638e5e40adbb39e49c0db8";
 for (let i = 0; i < eth.accounts.length; i++) {
     const account = eth.accounts[i];
     personal.unlockAccount(account, "123", 0);
